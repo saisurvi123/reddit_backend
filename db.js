c5c04d4f8d362bd0df8692e4c9ba19f56e241e0a
saikiran survi <survisaikiran7353@gmail.com>
2023-01-30 07:29:51 +0530

    connection change

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mongoose=require('mongoose');
 // const mongoURI="mongodb://localhost:27017/Reddit";
-const mongoURI="mongodb+srv://greddit:saikiran@cluster0.vutilli.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI="mongodb+srv://greddit:saikiran@cluster0.vutilli.mongodb.net/?retryWrites=true&w=majority/greddit"
 // this is how we connect to mongo
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to database");
+    mongoose.connect(mongoURI).then(()=>{
+        console.log("connected to db")
+    }).catch((err)=>{
+        console.log("unable to connect");
+        console.log(err)
     })
 }
 
